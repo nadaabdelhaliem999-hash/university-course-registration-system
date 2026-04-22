f06cee77fb7d63bd2adbd9d0a78180707df26f36
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-22 12:21:13 +0200

    Create Database.py

---
+++ Database.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def get_connection():
+    connection = sqlite3.connect("university.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def create_users_table():
+    connection = get_connection()
+    connection.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id       INTEGER PRIMARY KEY AUTOINCREMENT,
+            name     TEXT    NOT NULL,
+            email    TEXT    NOT NULL UNIQUE,
+            password TEXT    NOT NULL
+        )
+    """)
+    connection.commit()
+    connection.close()
+    print("Users table created!")
+
+if __name__ == "__main__":
+    create_users_table()
