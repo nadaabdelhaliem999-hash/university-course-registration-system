dd21c24194f8c88d4356824bbdc43ba8ec0d5c0f
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-24 14:31:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from flask import Flask, render_template, request, redirect, session
 from database import get_connection, create_users_table
 
