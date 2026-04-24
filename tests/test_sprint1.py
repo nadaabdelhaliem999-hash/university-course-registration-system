98bb6583f1e877e21e7828a763bbd109d821986b
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-24 13:36:28 +0300

    Update test_sprint1.py

--- tests/test_sprint1.py
+++ tests/test_sprint1.py
@@ -2,7 +2,7 @@ import pytest
 import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app
-import Database  # Import your database module
+
 
 @pytest.fixture
 def client():
