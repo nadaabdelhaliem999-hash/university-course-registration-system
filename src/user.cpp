e75447b3bd98a52ef5105485d6947a49c5731f75
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-27 22:01:41 +0300

    Update user.cpp

--- src/user.cpp
+++ src/user.cpp
@@ -1,11 +1,11 @@
 #include "user.h"
 #include <string>
 // Check if email contains @ symbol
-bool isValidEmail(const std::string& email) {
+bool isValidEmail(std::string& email) {
     return email.find('@') != std::string::npos;
 }
 
 // Check if password is at least 6 characters
-bool isValidPassword(const std::string& password) {
+bool isValidPassword(std::string& password) {
     return password.length() >= 6;
 }
