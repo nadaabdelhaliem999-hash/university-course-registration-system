c90ebaa6818a6173da65651e5e554a4b233bae79
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-27 16:26:23 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,9 +4,9 @@
 int main() {
     assert(isValidEmail("test@gmail.com") == true);
     assert(isValidEmail("wrong") == false);
-
+    
     assert(isValidPassword("123456") == true);
     assert(isValidPassword("123") == false);
-
+    
     return 0;
 }
