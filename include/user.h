f4c64da96f5b194f5322fd314cc68f702fb9ebc9
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-27 22:08:38 +0300

    Update user.h

--- include/user.h
+++ include/user.h
@@ -1,15 +1,16 @@
 #ifndef USER_H
 #define USER_H
-#include <string>
 
+#include <string>
+#include <string_view>  
 
 struct User {
     std::string email;
     std::string password;
 };
 
-// Also fix here (combine with previous issue)
-bool isValidEmail(const std::string& email);
-bool isValidPassword(const std::string& password);
+// changed to string_view
+bool isValidEmail(std::string_view email);
+bool isValidPassword(std::string_view password);
 
 #endif
