c1a9ae1116ae240c4c52c3a629a48eeb9f65ff3d
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-27 15:29:59 +0300

    Create user.h

---
+++ include/user.h
@@ -0,0 +1,15 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+using namespace std;
+
+struct User {
+    string email;
+    string password;
+};
+
+bool isValidEmail(string email);
+bool isValidPassword(string password);
+
+#endif
