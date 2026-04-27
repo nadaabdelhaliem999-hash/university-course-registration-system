9dba0eb075ad186e0f2563357bfb073c882f5338
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-27 15:38:49 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,18 +1,9 @@
 #include <cassert>
-#include <string>
-using namespace std;
-
-bool isValidEmail(string email) {
-    return email.find('@') != string::npos;
-}
-
-bool isValidPassword(string password) {
-    return password.length() >= 6;
-}
+#include "user.h"
 
 int main() {
     assert(isValidEmail("test@gmail.com") == true);
-    assert(isValidEmail("wrongemail") == false);
+    assert(isValidEmail("wrong") == false);
 
     assert(isValidPassword("123456") == true);
     assert(isValidPassword("123") == false);
