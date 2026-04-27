c9d809ce65ceff07862c2969dcdfc8b666735ce4
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-04-27 15:37:26 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include "user.h"
+
+using namespace std;
+
+int main() {
+    vector<User> users;
+
+    string email, password;
+
+    cout << "=== Registration ===\n";
+    cout << "Enter email: ";
+    cin >> email;
+
+    if (!isValidEmail(email)) {
+        cout << " Invalid email\n";
+        return 0;
+    }
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    if (!isValidPassword(password)) {
+        cout << " Password must be at least 6 characters\n";
+        return 0;
+    }
+
+    users.push_back({email, password});
+    cout << " Registered successfully\n";
+
+    return 0;
+}
