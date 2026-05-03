52f5ac08715a94ab63ebaa336e7bd3a51f3c9eb4
nadaabdelhaliem999-hash <nadaabdelhaliem999@gmail.com>
2026-05-03 15:15:03 +0300

    Create menu.cpp

---
+++ src/menu.cpp
@@ -0,0 +1,65 @@
+#include "user.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool handleGuestMenu(int choice,
+                     vector<User>& users,
+                     vector<Course>& courses,
+                     string& loggedInEmail) {
+    switch (choice) {
+        case 1:
+            handleRegister(users);
+            return true;
+        case 2:
+            handleLogin(users, loggedInEmail);
+            return true;
+        case 3:
+            handleViewCourses(courses);
+            return true;
+        case 4:
+            handleSearchCourses(courses);
+            return true;
+        case 5:
+            cout << "Goodbye!\n";
+            return false;
+        default:
+            cout << "ERROR: Invalid choice!\n";
+            return true;
+    }
+}
+
+bool handleLoggedInMenu(int choice,
+                        vector<User>& users,
+                        vector<Course>& courses,
+                        string& loggedInEmail) {
+    switch (choice) {
+        case 1:
+            handleViewCourses(courses);
+            return true;
+        case 2:
+            handleSearchCourses(courses);
+            return true;
+        case 3:
+            handleRegisterForCourse(users, loggedInEmail, courses);
+            return true;
+        case 4:
+            handleDropCourse(users, loggedInEmail, courses);
+            return true;
+        case 5:
+            handleViewSchedule(users, loggedInEmail, courses);
+            return true;
+        case 6:
+            cout << "Logged out successfully!\n";
+            loggedInEmail = "";
+            return true;
+        case 7:
+            cout << "Goodbye!\n";
+            return false;
+        default:
+            cout << "ERROR: Invalid choice!\n";
+            return true;
+    }
+}
